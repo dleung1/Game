3ec5c35662bb7a845f0df4ef99cb9c7f47f8bd8d
dleung1 <dleung1@mail.csuchico.edu>
2015-06-07 12:01:27 -0700

    test

---
+++ Window.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+import java.awt.Canvas;
+import javax.swing.JFrame;
+import java.awt.Dimension;
+
+public class Window extends Canvas { 
+    
+    private static final long serialVersionUID = -240080600;
+    
+    public Window(int width, int height, String title, Game game){
+        JFrame frame = new JFrame(title);
+        
+        frame.setPreferredSize(new Dimension(width, height));
+        frame.setMaximumSize(new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.add(game);
+        frame.setVisible(true);
+        game.start();
+    }
+    
+}
+
